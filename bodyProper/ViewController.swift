de5efd9a2336bd86484351888282912ffaec21d0
saturn777 <aleksandr-luchinin@bk.ru>
2022-11-07 16:51:05 -0800

    Initial Commit

---
+++ bodyProper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bodyProper
+//
+//  Created by Aleksandr Luchinin on 07.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
