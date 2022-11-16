5d31d5ce37707b3dcfb591e4dcecfe04cc5d6eeb
saturn777 <aleksandr-luchinin@bk.ru>
2022-11-15 17:15:16 -0800

    Создаю TabBarController

---
+++ bodyProper/NavigationModule/MainTabBarController.swift
@@ -0,0 +1,42 @@
+//
+//  MainTabBarController.swift
+//  bodyProper
+//
+//  Created by Aleksandr Luchinin on 15.11.2022.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupTabBar()
+        setupItems()
+        
+    }
+    private func setupTabBar() {
+        tabBar.backgroundColor = .specialTabBar
+        tabBar.tintColor = .specialDarkGreen //когда нажата кнопка
+        tabBar.unselectedItemTintColor = .specialGray
+        tabBar.layer.borderWidth = 1
+        tabBar.layer.borderColor = UIColor.specialLightBrown.cgColor
+    }
+    private func setupItems() {
+        let mainVC = MainViewController()
+        let statisticVC = StatisticViewController()
+        
+        setViewControllers([mainVC, statisticVC], animated: true)
+        
+        guard let items = tabBar.items else { return }
+        
+        items[0].title = "Main"
+        items[1].title = "Statistic"
+        
+        items[0].image = UIImage(named: "mainTabBar")
+        items[1].image = UIImage(named: "statisticTabBar")
+        
+        UITabBarItem.appearance().setTitleTextAttributes([.font : UIFont(name: "Roboto-Bold", size: 12) as Any], for: .normal)
+    }
+}
